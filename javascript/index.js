0922805baf6efb3f9a3ac320540ad9fe70bd3f63
Immaculate Okoruwa <immaculatechukwuemeka@gmail.com>
2023-08-14 17:13:36 -0500

    Added functionalities for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,66 @@
 function updateTime() {
   // Des Moines
   let desMoinesElement = document.querySelector("#des-moines");
-  let desMoinesDateElement = desMoinesElement.querySelector(".date");
-  let desMoinesTimeElement = desMoinesElement.querySelector(".time");
-  let desMoinesTime = moment().tz("America/Chicago");
+  if (desMoinesElement) {
+    let desMoinesDateElement = desMoinesElement.querySelector(".date");
+    let desMoinesTimeElement = desMoinesElement.querySelector(".time");
+    let desMoinesTime = moment().tz("America/Chicago");
 
-  desMoinesDateElement.innerHTML = desMoinesTime.format("MMMM Do YYYY");
-  desMoinesTimeElement.innerHTML = desMoinesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    desMoinesDateElement.innerHTML = desMoinesTime.format("MMMM Do YYYY");
+    desMoinesTimeElement.innerHTML = desMoinesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Ontario
   let ontarioElement = document.querySelector("#ontario");
-  let ontarioDateElement = ontarioElement.querySelector(".date");
-  let ontarioTimeElement = ontarioElement.querySelector(".time");
-  let ontarioTime = moment().tz("Canada/Eastern");
+  if (ontarioElement) {
+    let ontarioDateElement = ontarioElement.querySelector(".date");
+    let ontarioTimeElement = ontarioElement.querySelector(".time");
+    let ontarioTime = moment().tz("Canada/Eastern");
 
-  ontarioDateElement.innerHTML = ontarioTime.format("MMMM Do YYYY");
-  ontarioTimeElement.innerHTML = ontarioTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    ontarioDateElement.innerHTML = ontarioTime.format("MMMM Do YYYY");
+    ontarioTimeElement.innerHTML = ontarioTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Lagos
   let lagosElement = document.querySelector("#lagos");
-  let lagosDateElement = lagosElement.querySelector(".date");
-  let lagosTimeElement = lagosElement.querySelector(".time");
-  let lagosTime = moment().tz("Africa/Lagos");
+  if (lagosElement) {
+    let lagosDateElement = lagosElement.querySelector(".date");
+    let lagosTimeElement = lagosElement.querySelector(".time");
+    let lagosTime = moment().tz("Africa/Lagos");
 
-  lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
-  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+    lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+    lagosTimeElement.innerHTML = lagosTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
